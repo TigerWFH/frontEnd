f090a5b5d8d40694d72651b0366bc1d6c5233fbb
monkey <monkey@lwork.com>
2016-11-25 14:53:49 +0800

    初步添加button控件

---
+++ src/components/basic/button.tsx
@@ -0,0 +1,43 @@
+// libs
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+interface ButtonProps {
+    text?: string;
+    className?: string;
+    onClick?: { (text: any, instance: any): void }
+}
+interface ButtonState {
+    text?: string;
+}
+
+export class Button extends React.Component<ButtonProps, ButtonState>{
+    static defaultProps = {
+        text: 'BUTTON'
+    };
+    // change button text
+    changeText = (text: string) => {
+        this.setState({
+            text: text
+        });
+    }
+    constructor(props: ButtonProps) {
+        super(props);
+        this.state = {
+            text: null
+        };
+    }
+    _onClick = (text: any, instance: any) => {
+        this.props.onClick && this.props.onClick(text, instance);
+    }
+    render() {
+        return (
+            <div>
+                <button className={this.props.className || 'btn'}
+                    onClick={this._onClick.bind(null, 'abc', this)}>
+                    {this.state.text || this.props.text}
+                </button>
+            </div>
+        )
+    }
+}
