4a3dc500fbb4df7f511c2dc31f781e3fe23265c6
monkey <monkey@lwork.com>
2016-12-05 19:04:08 +0800

    selet组件初步完成

---
+++ src/components/basic/select.tsx
@@ -0,0 +1,71 @@
+// libs
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+export interface SelectData {
+    label: string | number;
+    value: string | number;
+}
+
+interface SelectProps {
+    data?: Array<SelectData>;
+    onChange?: { (text: string, self: any): void };
+}
+interface SelectState {
+    selected?: boolean;
+}
+
+export class Select extends React.Component<SelectProps, SelectState>{
+    refs: any;
+    // 初始化列表
+    constructor(props: SelectProps) {
+        super(props);
+    }
+    // 外部调用接口列表
+    getSelectedItem = () => {
+        let options = this.refs.select.options;
+        if (options.length <= 0) {
+            return;
+        }
+        return {
+            label: this.refs.select.options[this.refs.select.selectedIndex].text,
+            value: this.refs.select.value
+        };
+    }
+    setSelectedItem = (selectedIndex: number) => {
+        if (selectedIndex < 0 || selectedIndex >= this.refs.select.options.length) {
+            return;
+        }
+
+        this.refs.select.selectedIndex = selectedIndex;
+    }
+    // 内部调用接口列表
+    _onChange = () => {
+        if (typeof this.props.onChange === 'function') {
+            this.props.onChange('abc', this);
+        }
+    }
+    _renderOptions = (data: Array<SelectData>) => {
+        let options: Array<React.ReactNode> = [];
+        if (data instanceof Array) {
+            data.forEach((item: SelectData) => {
+                let obj = <option value={item.value}>
+                    {item.label}
+                </option>;
+                options.push(obj);
+            })
+        }
+
+        return options;
+    }
+    render() {
+        return (
+            <div className="monkey-select-wrapper">
+                <select ref='select'
+                    onChange={this._onChange}>
+                    {this._renderOptions(this.props.data)}
+                </select>
+            </div>
+        )
+    }
+}
--- src/module1/index.tsx
+++ src/module1/index.tsx
@@ -11,6 +11,7 @@ import { AppContent } from '../components/appContent';
 import { View } from '../components/basic/view';
 import { Button } from '../components/basic/button';
 import { TextInput } from '../components/basic/input';
+import { Select } from '../components/basic/select';
 // models
 
 interface P { }
@@ -26,9 +27,15 @@ export class Module1 extends React.Component<P, S>{
     _onClickButton = (self: any) => {
         console.log('home-->', this.refs.home.getInputText());
         this.refs.home.setInputText('set')
+        console.log(this.refs.select);
+        console.log('select-->', this.refs.select.getSelectedItem());
+        this.refs.select.setSelectedItem(0);
     }
     _onChange = () => {
 
+    }
+    _onSelect = () => {
+        console.log(event);
     }
     render() {
         return (
@@ -44,6 +51,12 @@ export class Module1 extends React.Component<P, S>{
                             placeholder="123"
                             onChange={this._onChange}
                             />
+                        <Select ref="select"
+                            data={[
+                                { label: 'monkey', value: "888" },
+                                { label: 'cat', value: "666" },
+                                { label: 'elephant', value: "333" }
+                            ]} />
                         <AppFooter />
                     </View>
                 </AppContent>
